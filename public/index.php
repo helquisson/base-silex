265fc465466122ac8764e6ba5f5ed252f2b3d85d
helquisson <helquisson@hotmail.com>
2015-07-13 08:35:07 -0300

    add twig

---
+++ public/index.php
@@ -0,0 +1,57 @@
+<?php
+
+define('APP_ROOT', dirname(__DIR__));
+chdir(APP_ROOT);
+
+use Silex\Application;
+
+//use Symfony\Component\HttpFoundation\Request;
+//use Symfony\Component\HttpFoundation\Response;
+
+require 'vendor/autoload.php';
+
+$app = new Application();
+
+$app['debug'] == true;
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => 'views'
+));
+
+/* exemplo de middleware before */
+
+/*$app->before(function (Request $request) {
+    print 'Antes das rotas - ';
+});*/
+
+$app->get('/', function () use ($app) {
+    return $app->json(array("Hello World - "));
+});
+
+/* exemplo de middleware after */
+
+/*$app->after(function (Request $request, Response $response) {
+    print 'After';
+});*/
+
+/* exemplo de middleware finish */
+
+/*$app->finish(function (Request $request, Response $response) {
+    print 'depois de response enviado';
+});*/
+
+/* rotas */
+$app->get('/users/{name}/{id}', function ($name, $id) use ($app) {
+
+    if (is_null($name) || is_null($id)):
+        return $app->redirect('/');
+    endif;
+
+    return $app['twig']->render('index.twig', array(
+        'name' => $name,
+        'id' => $id
+    ));
+})->value('name', NULL)
+    ->value('id', NULL);
+
+$app->run();
